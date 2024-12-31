646e3133dd6b3e1a9eaca3b0f51f1e4875ec71d8
jim-les <jimlest35@gmail.com>
2024-12-31 10:18:10 +0300

    updated the allowed hosts

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('cart/', include('cart.urls', namespace='cart')),
